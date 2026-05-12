d0ede179cf30e4fe2524622c66d3d92b44c63056
PavanSaiNekkala <91945047+PavanSaiNekkala@users.noreply.github.com>
2026-05-12 17:31:08 +0530

    Update financial_extractor.py

--- core/financial_extractor.py
+++ core/financial_extractor.py
@@ -1,1 +1,33 @@
+import yfinance as yf
+import numpy as np
 
+def extract_financials(symbol):
+
+    ticker = yf.Ticker(symbol)
+
+    financials = ticker.financials
+
+    data = {
+
+        "Revenue": np.nan,
+        "Net Income": np.nan,
+        "EBITDA": np.nan
+
+    }
+
+    try:
+        data["Revenue"] = financials.loc["Total Revenue"].iloc[0]
+    except:
+        pass
+
+    try:
+        data["Net Income"] = financials.loc["Net Income"].iloc[0]
+    except:
+        pass
+
+    try:
+        data["EBITDA"] = financials.loc["EBITDA"].iloc[0]
+    except:
+        pass
+
+    return data
