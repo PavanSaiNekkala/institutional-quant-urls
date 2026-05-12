ff34769ae4d30bc47c7b93b64e3c585a8f2c57c2
PavanSaiNekkala <91945047+PavanSaiNekkala@users.noreply.github.com>
2026-05-12 17:31:36 +0530

    Update cashflow_extractor.py

--- core/cashflow_extractor.py
+++ core/cashflow_extractor.py
@@ -1,1 +1,33 @@
+import yfinance as yf
+import numpy as np
 
+def extract_cashflow(symbol):
+
+    ticker = yf.Ticker(symbol)
+
+    cashflow = ticker.cashflow
+
+    data = {
+
+        "Operating Cashflow": np.nan,
+        "Free Cashflow": np.nan,
+        "Capex": np.nan
+
+    }
+
+    try:
+        data["Operating Cashflow"] = cashflow.loc["Operating Cash Flow"].iloc[0]
+    except:
+        pass
+
+    try:
+        data["Free Cashflow"] = cashflow.loc["Free Cash Flow"].iloc[0]
+    except:
+        pass
+
+    try:
+        data["Capex"] = cashflow.loc["Capital Expenditure"].iloc[0]
+    except:
+        pass
+
+    return data
